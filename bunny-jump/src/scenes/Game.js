a82c071c40a0840ce32e47f76ce801442d5e682e
wrileyb <wrilsb@uw.edu>
2023-10-18 19:38:00 -0700

    Added progress with phaser tutorial

---
+++ bunny-jump/src/scenes/Game.js
@@ -0,0 +1,31 @@
+import Phaser from '../lib/phaser.js'
+export default class Game extends Phaser.Scene
+{
+    constructor()
+    {
+        super('game')
+    }
+
+    preload()
+    {
+        this.load.image('background', 'assets/bg_layer1.png')
+        this.load.image('platform', 'assets/ground_grass.png')
+    }
+    create()
+    {
+        this.add.image(240, 320, 'background')
+        const platforms = this.physics.add.staticGroup()
+    // then create 5 platforms from the group
+        for (let i = 0; i < 5; ++i)
+        {
+            const x = Phaser.Math.Between(80, 400)
+            const y = 150 * i
+        /** @type {Phaser.Physics.Arcade.Sprite} */
+        const platform = platforms.create(x, y, 'platform')
+        platform.scale = 0.5
+        /** @type {Phaser.Physics.Arcade.StaticBody} */
+        const body = platform.body
+        body.updateFromGameObject()
+23      }
+    }
+}
